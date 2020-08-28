121abb5428d44786242c654b6acd658db72f71fc
Abner Willys <abwillys001@gmail.com>
2020-08-28 18:41:18 -0400

    Starting the project Launcstore on class of modules 06

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express  = require('express')
+const nunjucks = require('nunjucks')
+const routes   = require('./routes')
+const methodOverride = require('method-override')
+
+const server = express()
+
+//Middleware
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static("public"))
+server.use(methodOverride('_method'))
+server.use(routes)
+
+//Setting view engine (nunjucks)
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+    watch: true
+})
+
+//Server door
+server.listen(5000, () => {
+    console.log("Server is Running")
+})
