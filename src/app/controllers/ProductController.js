60b713534bf45e9dea4a9d421bf4febb64a8a00a
Abner Willys <abwillys001@gmail.com>
2020-09-08 00:21:48 -0400

    End of Module 07

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice } = require('../../lib/useful')
+const { formatPrice, date } = require('../../lib/useful')
 
 const Category = require("../models/Category")
 const Product  = require("../models/Product")
@@ -39,6 +39,31 @@ module.exports = {
     await Promise.all(filesPromise)
 
     return res.redirect(`/products/${productId}/edit`)
+  },
+  async show(req, res) {
+
+    let results   = await Product.find(req.params.id)
+    const product = results.rows[0]
+    
+    if (!product) return res.send("Product Not Found!")
+
+    const { month, day, hour, minutes } = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h ${minutes}`
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price    = formatPrice(product.price)
+
+    results = await Product.files(product.id)
+    const files = results.rows.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+    })) 
+
+    return res.render('products/show', { product, files })
   },
   async edit(req, res) {
     let results = await Product.find(req.params.id)
@@ -95,7 +120,6 @@ module.exports = {
       await Promise.all(newFilesPromise)
     }
 
-
     req.body.price = req.body.price.replace(/\D/g, "")//R$ 1,00 = 100
 
     //Update of old_price;
@@ -107,7 +131,7 @@ module.exports = {
 
     await Product.update(req.body)
 
-    return res.redirect(`products/${req.body.id}/edit`)
+    return res.redirect(`products/${req.body.id}`)
   },
   async delete(req, res) {
     await Product.delete(req.body.id)
